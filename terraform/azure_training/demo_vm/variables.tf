6e616bcf4a3ac72d2e40afafbcebcf2d2c20d98e
Serhii <simplyguru-dot@users.noreply.github.com>
2020-03-25 14:22:06 +0100

    Merging

---
+++ terraform/azure_training/demo_vm/variables.tf
@@ -0,0 +1,7 @@
+# Variables
+
+variable "vm_type" {
+  type = string
+  default = "Standard_DS1_v2"
+  description = "Default VM type"
+}
---
+++ terraform/azure_training/scale/main.tf
@@ -0,0 +1,59 @@
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x.
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "azure-training"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = var.prefix
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "main" {
+  name                            = var.prefix
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  sku                             = "Standard_B1ls"
+  instances                       = 3
+  admin_username                  = "adminuser"
+  admin_password                  = "P@ssw0rd1234!"
+  disable_password_authentication = false
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  network_interface {
+    name    = "example"
+    primary = true
+
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = azurerm_subnet.internal.id
+    }
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
